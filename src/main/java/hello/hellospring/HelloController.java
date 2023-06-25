67889e3e3be94f7f056f6f9c1be52f0413407b48
iian <dheyw@naver.com>
2023-06-26 00:35:14 +0900

    Spring 연습

--- src/main/java/hello/hellospring/HelloController.java
+++ src/main/java/hello/hellospring/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
-    @GetMapping("/sbb")
+    @GetMapping("/sbb") // url함수 호출
     @ResponseBody  // 문자열 인식 어노테이션
     public String sbb(){
         return "안녕하세요 sbb에 오신것을 환영합니다.";
