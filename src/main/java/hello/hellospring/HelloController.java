c9b7536ecab972679be123184d6201cbc06609d4
iian <dheyw@naver.com>
2023-06-26 00:23:05 +0900

    Spring 연습

---
+++ src/main/java/hello/hellospring/HelloController.java
@@ -0,0 +1,15 @@
+package hello.hellospring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/sbb")
+    @ResponseBody  // 문자열 인식 어노테이션
+    public String sbb(){
+        return "안녕하세요 sbb에 오신것을 환영합니다.";
+    }
+}
