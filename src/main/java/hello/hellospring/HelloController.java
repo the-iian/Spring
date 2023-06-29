b33ab0b65800354a9b2d7e123c56fd096ac9ed35
iian <dheyw@naver.com>
2023-06-29 23:31:14 +0900

    getmapping

--- src/main/java/hello/hellospring/HelloController.java
+++ src/main/java/hello/hellospring/HelloController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@Controller // url을 맵핑
 public class HelloController {
 
-    @GetMapping("/sbb") // url함수 호출
+    @GetMapping("/sbb") // url함수 호출, url요청할때 사용
     @ResponseBody  // 문자열 인식 어노테이션
     public String sbb(){
         return "안녕하세요 sbb에 오신것을 환영합니다.";
--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
 	// 프로젝트 생성 연습
